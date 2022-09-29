35a019eae70033ce76823c43fa424e6e2275125b
dhakad-111 <sndpatel1994@gmail.com>
2022-09-29 19:10:44 +0530

    create swagger ui for api's

---
+++ task2/Microservices/src/main/java/com/microservices/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.microservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.microservices"))
+				.paths(PathSelectors.any()).build().apiInfo(Metadata());
+	}
+
+	private ApiInfo Metadata() {
+		return new ApiInfoBuilder().title("microservices {} - spring boot Swagger configuration")
+				.description("\" Swagger configuration for application\"").version("1.0.0").license("Apache 2.0")
+				.contact(new Contact("Surendra Dhakad", "https://github.com/dhakad-111", "dsurendradhakad@gmail.com"))
+				.build();
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
